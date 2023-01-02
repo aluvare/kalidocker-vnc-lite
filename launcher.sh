8d2cccfe414287b6b84ab2d48d7b2d934ec81af4
aluvare <luvare@gmail.com>
2023-01-02 19:31:25 +0100

    PRE_COMMAND

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,7 @@
 if [[ "$DOCKER" != "true" ]];then
         rm -rf /etc/supervisor/conf.d/docker.conf
 fi
+if [[ "${PRE_COMMAND}" != "" ]];then
+	echo ${PRE_COMMAND} | base64 -d | bash
+fi
 supervisord
