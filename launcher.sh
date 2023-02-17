e31472770eaad69ea0ed466a76a24c54228dbd1d
aluvare <luvare@gmail.com>
2023-02-17 21:44:20 +0100

    fix

--- launcher.sh
+++ launcher.sh
@@ -4,4 +4,5 @@ fi
 if [[ "$PRECOMMAND" != "" ]];then
 	echo $PRECOMMAND | base64 -d | bash
 fi
+bash -c 'sleep 10 && sed -i "s/.*\"cpugraph\".*//g" /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml && sed -i "s/.*\"power-manager-plugin\".*//g" /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml && supervisorctl restart xfce4' &
 supervisord
